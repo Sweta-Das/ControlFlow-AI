66eb2ed1ff934a25442d91c6d28286c31d8bd983
Sweta-Das <dassweta1997@gmail.com>
2025-08-05 12:11:36 +0545

    tested litellm config

--- pydantic-ai/examples/1_hello_world.py
+++ pydantic-ai/examples/1_hello_world.py
@@ -1,17 +1,41 @@
-from pydantic_ai import Agent
+# from pydantic_ai import Agent
+# from dotenv import find_dotenv, load_dotenv
+
+# load_dotenv(find_dotenv())
+
+# agent = Agent(  
+#     model='google-gla:gemini-1.5-flash',
+#     system_prompt='Be concise, reply with one sentence.',  
+# )
+
+# result = agent.run_sync('Where does "hello world" come from?')  
+# print(result.output)
+
+
+# """
+# The phrase "hello, world" originated in Brian Kernighan's 1972 "A Tutorial Introduction to the Language B".
+# """
+import os
+from pydantic_ai.providers.openai import OpenAIProvider
+
 from dotenv import find_dotenv, load_dotenv
 
 load_dotenv(find_dotenv())
 
-agent = Agent(  
-    model='google-gla:gemini-1.5-flash',
-    system_prompt='Be concise, reply with one sentence.',  
+from pydantic_ai import Agent
+from pydantic_ai.models.openai import OpenAIModel
+
+
+model = OpenAIModel(
+    "gemini/gemini-2.5-flash-lite",
+    provider=OpenAIProvider(
+        base_url=os.getenv("LITELLM_API_BASE"),
+        api_key=os.getenv("LITELLM_API_KEY"),
+    ),
 )
+agent = Agent(model, system_prompt="What is the capital of France?")
 
-result = agent.run_sync('Where does "hello world" come from?')  
+
+
+result = agent.run_sync()
 print(result.output)
-
-
-"""
-The phrase "hello, world" originated in Brian Kernighan's 1972 "A Tutorial Introduction to the Language B".
-"""
